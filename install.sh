50180e476e75f9588849781ba5e04562c0d42691
VirusHQ <35165099+VirusHQ@users.noreply.github.com>
2018-11-30 01:58:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 TMP_DIR=/tmp/ethos-ubqminer
 mkdir $TMP_DIR
-cd /tmp/ethos-ubqminer
+cd $TMP_DIR
 git clone https://github.com/VirusHQ/ethos-ubqminer.git
 cp -r $TMP_DIR/ethos-ubqminer/opt/* /opt/
 wget https://github.com/ubiq/ubqminer/releases/download/v0.17.0-alpha.1.ubqhash/ubqminer-0.17.0-alpha.1.ubqhash-cuda10.0-linux-x86_64.tar.gz
